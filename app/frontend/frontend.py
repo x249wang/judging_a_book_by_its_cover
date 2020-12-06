0e6418f8a0e290b6816a5ec21eb7613be7daf2b7
x249wang <x249wang@uwaterloo.ca>
2020-12-06 00:39:41 -0500

    Adjust UI layout

--- app/frontend/frontend.py
+++ app/frontend/frontend.py
@@ -2,6 +2,11 @@ import streamlit as st
 import requests
 from PIL import Image
 
+# Hacky way to center images as customizable layout is currently not supported by Streamlit
+# (source: https://discuss.streamlit.io/t/how-to-center-images-latex-header-title-etc/1946/4)
+with open("style.css") as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
 st.title("Judging a Book by Its Cover")
 
 uploaded_file = st.file_uploader(
@@ -11,23 +16,21 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
 
-    column1, column2, column3, column4 = st.beta_columns(4)
-
     # Show uploaded image
     image = Image.open(uploaded_file)
     image_width = min(image.size[1], 400)
-    column2.image(image, caption="Your Book Cover", width=image_width)
+    st.image(image, caption="Your Book Cover", width=image_width)
 
     # Query for model prediction
     file = {"file": uploaded_file.getvalue()}
     response = requests.post("http://backend:8080/judge", files=file)
 
     if response.status_code != requests.codes.ok:
-        column3.markdown("Invalid format - image must be in RGB mode!")
+        st.markdown("Invalid format - image must be in RGB mode!")
 
     else:
         predicted_book_rating = response.json()["predicted_book_rating"]
-        column3.markdown(
+        st.markdown(
             f"<br/><br/>Predicted Rating: <b>{predicted_book_rating}</b>",
             unsafe_allow_html=True,
         )
